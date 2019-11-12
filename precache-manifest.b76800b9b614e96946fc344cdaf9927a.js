03f4b70f14d7e967e695b085ce3c3700e25db2ef
T3k5upp0rt <kylan.childers@gmail.com>
2019-11-11 16:13:44 -0800

    Updates

---
+++ precache-manifest.b76800b9b614e96946fc344cdaf9927a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "121fb632512f9fa64587fec7d0595e27",
+    "url": "/react-memory-game/index.html"
+  },
+  {
+    "revision": "87bccd877de8fb8787fe",
+    "url": "/react-memory-game/static/css/main.64869254.chunk.css"
+  },
+  {
+    "revision": "924987bbeca84f4ea166",
+    "url": "/react-memory-game/static/js/2.1a358605.chunk.js"
+  },
+  {
+    "revision": "87bccd877de8fb8787fe",
+    "url": "/react-memory-game/static/js/main.05b919a7.chunk.js"
+  },
+  {
+    "revision": "481192a02634cc708fe3",
+    "url": "/react-memory-game/static/js/runtime-main.541f3236.js"
+  }
+]);
